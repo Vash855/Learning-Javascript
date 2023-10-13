b207d614cb7eb7024e6d97ca23ceed10ecb938d4
Vash855 <rodneyames543@gmail.com>
2023-10-13 18:11:30 -0400

    third commit

--- rockPaperScissors/app.js
+++ rockPaperScissors/app.js
@@ -8,7 +8,7 @@ if (aiMove >= 0 && aiMove < 1/3) {
 	aiMove = 'Scissors';
 }
 
-console.log(aiMove);
+/*console.log(aiMove);
 
 let result = ''
 
@@ -19,4 +19,37 @@ if (aiMove === 'Rock') {
 } else if (aiMove ==='Scissors') {
 	result = 'You Win!';
 }
-console.log(result);
+console.log(result);*/
+
+
+playGame=(playerMove)=>{
+	let result = ''
+	if (playerMove === 'Rock') {
+		if (aiMove === 'Rock') {
+			result = 'Tie.';
+		} else if (aiMove === 'Paper') {
+			result ='You Lose!';
+		} else if (aiMove ==='Scissors') {
+			result = 'You Win!';
+		}
+	} else if(playerMove === 'Paper') {
+		if (aiMove === 'Rock') {
+			result = 'You Win!';
+		} else if (aiMove === 'Paper') {
+			result ='Tie.';
+		} else if (aiMove ==='Scissors') {
+			result = 'You Lose!';
+		}
+	} else if (playerMove === 'Scissors') {
+		if (aiMove === 'Rock') {
+			result = 'You Lose!';
+		} else if (aiMove === 'Paper') {
+			result ='You Win!';
+		} else if (aiMove ==='Scissors') {
+			result = 'Tie.';
+		}
+	} console.log(`You chose ${playerMove}. Computer chose ${aiMove} ${result}`);
+}
+
+
+playGame('Paper')
